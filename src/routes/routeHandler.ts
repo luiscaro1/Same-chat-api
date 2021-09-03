a2c05b5dd82c2107de7e29dff6d6f0838ba03026
Luis Caro <luis.caro1@upr.edu>
2021-09-03 13:31:49 -0300

    Adding welcome message when going to the root route

--- src/routes/routeHandler.ts
+++ src/routes/routeHandler.ts
@@ -3,4 +3,8 @@ import inboxRouter from "routes/inbox";
 
 export default function routeHandler(app: express.Application): void {
   app.use("/inbox", inboxRouter);
+
+  app.get("/", (req: express.Request, res: express.Response) => {
+    res.send(`Welcome to Same's Chat API`);
+  });
 }
