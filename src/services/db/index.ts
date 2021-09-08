33b5290e2ac59b265bef80a2fa304bb85a8a4efb
Luis <luis.caro1@upr.edu>
2021-09-08 19:59:56 -0400

    changed dockerfile name

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,4 +1,14 @@
 import knexfile from "services/db/knexfile";
 import knex, { Knex } from "knex";
+import Injectable from "services/decorators/injectable";
 
-const db = knex(knexfile);
+@Injectable("dbContext")
+export default class DbContext {
+  private db: Knex;
+
+  constructor() {
+    this.db = knex(knexfile);
+  }
+
+  // TODO: insert getters and setters for the different tables
+}
--- src/services/decorators/inject.ts
+++ src/services/decorators/inject.ts
@@ -1,7 +1,7 @@
 import container from "services/container";
 
 export default function Inject(token: string) {
-  return function (target: any, key: string) {
+  return function (target: any, key: string): void {
     Object.defineProperty(target, key, {
       get: () => container.resolve(token),
       enumerable: true,
